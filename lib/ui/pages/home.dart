b62ab71052988c133edd0f7fcb3f54301a365105
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-09 23:27:31 +0530

    section headers done

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:elearning/theme/box_icons_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -111,17 +112,19 @@ class HomePage extends StatelessWidget {
           SafeArea(
             child: CustomScrollView(
               slivers: <Widget>[
+                SliverFixedExtentList(
+                    delegate: SliverChildListDelegate.fixed([Container()]),
+                    itemExtent: MediaQuery.of(context).size.height * 0.29),
                 SliverToBoxAdapter(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Show Favorites Only',
-                        style: TextStyle().copyWith(
-                          fontSize: 17.0,
-                        ),
-                      ),
-                    ],
+                  child: SectionHeader(
+                    text: 'Recommended Lectures',
+                    onPressed: () {},
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: SectionHeader(
+                    text: 'Revision Lectures',
+                    onPressed: () {},
                   ),
                 ),
               ],
@@ -133,6 +136,43 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class SectionHeader extends StatelessWidget {
+  const SectionHeader({
+    Key key,
+    @required this.text,
+    @required this.onPressed,
+  }) : super(key: key);
+  final String text;
+  final Function onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 20,
+          width: 4,
+          margin: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(500),
+              color: Color(0xFF343434)),
+        ),
+        Text(
+          text,
+          style: TextStyle().copyWith(
+            fontSize: 17.0,
+          ),
+        ),
+        Spacer(),
+        CupertinoButton(
+          child: Icon(BoxIcons.bx_chevron_right, color: Colors.lightBlue),
+          onPressed: onPressed,
+        )
+      ],
     );
   }
 }
