49c5bc059c44cb03595c05afd85e329f07d4ec25
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-10 00:08:29 +0530

    created videocards

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:elearning/theme/box_icons_icons.dart';
+import 'package:elearning/ui/widgets/card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -122,6 +123,19 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 SliverToBoxAdapter(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 240,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return VideoCard();
+                      },
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
                   child: SectionHeader(
                     text: 'Revision Lectures',
                     onPressed: () {},
@@ -135,6 +149,98 @@ class HomePage extends StatelessWidget {
             child: TopBar(controller: controller),
           )
         ],
+      ),
+    );
+  }
+}
+
+class VideoCard extends StatelessWidget {
+  const VideoCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: CardWidget(
+        gradient: false,
+        button: true,
+        width: 180,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: 180,
+              height: 90,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/video.jpg'),
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: Text(""),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Stars - What are they made up of ?",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Color(0xFF535353),
+                    fontFamily: 'Red Hat Display',
+                    fontSize: 16),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(BoxIcons.bx_bar_chart_alt_2, size: 16),
+                  Text(
+                    "Beginner",
+                    style: TextStyle(
+                        color: Color(0xFFADADAD),
+                        fontFamily: 'Red Hat Display',
+                        fontSize: 10),
+                  ),
+                  Spacer(),
+                  Text(
+                    "12 mins",
+                    style: TextStyle(
+                        color: Color(0xFFADADAD),
+                        fontFamily: 'Red Hat Display',
+                        fontSize: 10),
+                  ),
+                  Icon(BoxIcons.bx_timer, size: 16),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6.0),
+              child: CardWidget(
+                  gradient: true,
+                  button: true,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(BoxIcons.bx_play_circle, color: Colors.white),
+                      Text(
+                        "Watch Lecture",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Red Hat Display',
+                            fontSize: 18),
+                      )
+                    ],
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
