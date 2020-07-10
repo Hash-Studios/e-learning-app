c65153d133e7235f71d7a7ba6cd19c258e50aaf9
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-07-10 19:23:11 +0530

    added menu wave

--- lib/ui/pages/navmenu/menu.dart
+++ lib/ui/pages/navmenu/menu.dart
@@ -1,3 +1,4 @@
+import 'package:elearning/theme/box_icons_icons.dart';
 import 'package:elearning/ui/widgets/card.dart';
 import 'package:flutter/material.dart';
 
@@ -20,23 +21,34 @@ class Menu extends StatelessWidget {
     return Stack(
       children: <Widget>[
         Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.33,
-              height: MediaQuery.of(context).size.height * 0.67,
-              decoration: BoxDecoration(
-                color: Color(0x80FFFFFF),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            )
-            // CardWidget(
-            //   button: false,
-            //   gradient: true,
-            //   height: MediaQuery.of(context).size.height * 0.75,
-            //   width: MediaQuery.of(context).size.width * 0.35,
-            //   child: Text(""),
-            // ),
+          alignment: Alignment.centerRight,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.33,
+            height: MediaQuery.of(context).size.height * 0.67,
+            decoration: BoxDecoration(
+              color: Color(0x80FFFFFF),
+              borderRadius: BorderRadius.circular(10),
             ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 135,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/navwave.png"),
+                  fit: BoxFit.fitWidth),
+            ),
+          ),
+        ),
+        Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              icon: Icon(BoxIcons.bx_window_close),
+              onPressed: () {},
+            )),
         SlideTransition(
           position: slideAnimation,
           child: ScaleTransition(
@@ -49,67 +61,7 @@ class Menu extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        // BlocProvider.of<NavigationBloc>(context)
-                        //     .add(NavigationEvents.DashboardClickedEvent);
-                        // onMenuItemClicked();
-                      },
-                      child: Text(
-                        "Dashboard",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: selectedIndex == 0
-                              ? FontWeight.w900
-                              : FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: () {
-                        // BlocProvider.of<NavigationBloc>(context)
-                        //     .add(NavigationEvents.MessagesClickedEvent);
-                        // onMenuItemClicked();
-                      },
-                      child: Text(
-                        "Messages",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: selectedIndex == 1
-                              ? FontWeight.w900
-                              : FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: () {
-                        // BlocProvider.of<NavigationBloc>(context)
-                        //     .add(NavigationEvents.UtilityClickedEvent);
-                        // onMenuItemClicked();
-                      },
-                      child: Text(
-                        "Utility Bills",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: selectedIndex == 2
-                              ? FontWeight.w900
-                              : FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    Text("Funds Transfer",
-                        style: TextStyle(color: Colors.white, fontSize: 22)),
-                    SizedBox(height: 30),
-                    Text("Branches",
-                        style: TextStyle(color: Colors.white, fontSize: 22)),
-                  ],
+                  children: <Widget>[],
                 ),
               ),
             ),
