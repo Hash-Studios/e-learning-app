f971148e8cedbe3d63cb1c378058a7084c427a72
apyadav1999 <arpity.ec18@nsut.ac.in>
2020-07-11 21:29:31 +0530

    ui of settings

--- lib/ui/pages/setting.dart
+++ lib/ui/pages/setting.dart
@@ -3,6 +3,30 @@ import 'package:flutter/cupertino.dart';
 
 import '../widgets/card.dart';
 
+Widget _container(IconData icon, String title) {
+  return Container(
+    decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(15)),
+    height: 45,
+    // color: Colors.white,
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+      child: Row(children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.black,
+        ),
+        SizedBox(width: 20),
+        Text(title,
+            style: TextStyle(
+              color: Color(0xffADADAD),
+              fontSize: 25,
+            )),
+      ]),
+    ),
+  );
+}
+
 class Settings extends StatefulWidget {
   @override
   _SettingsState createState() => _SettingsState();
@@ -37,33 +61,33 @@ class _SettingsState extends State<Settings> {
       ),
       child: Container(
         color: Color(0xffE5E5E5),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              // CardWidget(gradient: false, button: true, child: Text('LOG OUT')),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15)),
-                height: 45,
-                // color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                  child: Row(children: <Widget>[
-                    Icon(Icons.supervised_user_circle),
-                    SizedBox(width: 20),
-                    Text('Log Out',
-                        style: TextStyle(
-                          color: Color(0xffADADAD),
-                          fontSize: 25,
-                        )),
-                  ]),
-                ),
-              ),
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CupertinoButton(
+                child: _container(Icons.supervised_user_circle, 'Logout'),
+                onPressed: null),
+            CupertinoButton(
+                child: _container(Icons.supervised_user_circle, 'Clear Data'),
+                onPressed: null),
+            CupertinoButton(
+                child: _container(Icons.supervised_user_circle, 'ads '),
+                onPressed: null),
+            CupertinoButton(
+                child: _container(
+                    Icons.supervised_user_circle, 'How you earn coins ?'),
+                onPressed: null),
+
+            CupertinoButton(
+                child: _container(Icons.supervised_user_circle, 'Logout'),
+                onPressed: null),
+            // CupertinoButton(
+            //     child: _container(Icons.supervised_user_circle, 'Logout'),
+            //     onPressed: null),
+            // CupertinoButton(child: Text('hello'), onPressed: null),
+            // CupertinoButton(child: Text('hello'), onPressed: null),
+            // CardWidget(gradient: false, button: true, child: Text('LOG OUT')),
+          ],
         ),
       ),
     );
